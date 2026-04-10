eba2f3f3c8be3df8545cc3eb8659dd489f597d70
trimechoumayna <vortexcipher85@gmail.com>
2026-04-10 14:43:28 +0100

    Update Puzzle.h

--- Puzzle.h
+++ Puzzle.h
@@ -3,34 +3,36 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <time.h>
 
-#define SCREEN_W 800
+#define GRID 3
+#define SCREEN_W 600
 #define SCREEN_H 600
-#define GRID 3   // 3x3 puzzle
 
+// -------- STRUCT --------
 typedef struct {
     int id;
     SDL_Rect rect;
 } Tile;
 
-// Variables globales externes
+// -------- GLOBALS --------
 extern SDL_Window *win;
 extern SDL_Renderer *ren;
 extern SDL_Texture *img;
 
 extern Tile tiles[GRID * GRID];
-extern int order[GRID * GRID];
-extern int selected;
+extern int order[GRID][GRID];
+
 extern Uint32 startTime;
 extern int winGame;
 
-// Fonctions
+// -------- FUNCTIONS --------
 SDL_Texture* loadImage(const char *path);
+
 void initPuzzle();
-int checkWin();
 void renderPuzzle();
-void swapTiles(int a, int b);
+int checkWin();
+void swapTiles(int x1, int y1, int x2, int y2);
+
 void showTimer();
 void winAnimation();
 
