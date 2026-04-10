cf4b3a09fcd4a8bb29384623d4f2dfd1997987b0
trimechoumayna <vortexcipher85@gmail.com>
2026-04-10 14:44:13 +0100

    Update Puzzle.c

--- Puzzle.c
+++ Puzzle.c
@@ -1,97 +1,131 @@
 #include "puzzle.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// Déclaration des variables globales
+// -------- GLOBAL VARIABLES --------
 SDL_Window *win;
 SDL_Renderer *ren;
 SDL_Texture *img;
 
 Tile tiles[GRID * GRID];
-int order[GRID * GRID];
+int order[GRID][GRID];
 
-int selected = -1;
 Uint32 startTime;
 int winGame = 0;
 
-// -------------------- LOAD IMAGE --------------------
+// -------- LOAD IMAGE --------
 SDL_Texture* loadImage(const char *path) {
     SDL_Surface *tmp = IMG_Load(path);
     if (!tmp) {
         printf("Erreur image: %s\n", path);
         exit(1);
     }
+
     SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, tmp);
     SDL_FreeSurface(tmp);
     return tex;
 }
 
-// -------------------- INIT PUZZLE --------------------
+// -------- INIT PUZZLE (MATRICE) --------
 void initPuzzle() {
-    int index = 0;
     int tileW = SCREEN_W / GRID;
     int tileH = SCREEN_H / GRID;
 
+    int index = 0;
+
+    // création ordonnée
     for (int y = 0; y < GRID; y++) {
         for (int x = 0; x < GRID; x++) {
+
             tiles[index].id = index;
+
             tiles[index].rect.x = x * tileW;
             tiles[index].rect.y = y * tileH;
             tiles[index].rect.w = tileW;
             tiles[index].rect.h = tileH;
-            order[index] = index;
+
+            order[y][x] = index;
             index++;
         }
     }
 
-    // Shuffle des tuiles
+    // shuffle matrice
     srand(time(NULL));
-    for (int i = 0; i < GRID * GRID; i++) {
-        int r = rand() % (GRID * GRID);
-        int tmp = order[i];
-        order[i] = order[r];
-        order[r] = tmp;
+
+    for (int y = 0; y < GRID; y++) {
+        for (int x = 0; x < GRID; x++) {
+
+            int ry = rand() % GRID;
+            int rx = rand() % GRID;
+
+            int tmp = order[y][x];
+            order[y][x] = order[ry][rx];
+            order[ry][rx] = tmp;
+        }
     }
 
     startTime = SDL_GetTicks();
 }
 
-// -------------------- CHECK WIN --------------------
+// -------- CHECK WIN --------
 int checkWin() {
-    for (int i = 0; i < GRID * GRID; i++) {
-        if (order[i] != i) return 0;
+    int expected = 0;
+
+    for (int y = 0; y < GRID; y++) {
+        for (int x = 0; x < GRID; x++) {
+            if (order[y][x] != expected)
+                return 0;
+            expected++;
+        }
     }
     return 1;
 }
 
-// -------------------- RENDER PUZZLE --------------------
+// -------- RENDER PUZZLE --------
 void renderPuzzle() {
     int tileW = SCREEN_W / GRID;
     int tileH = SCREEN_H / GRID;
 
-    for (int i = 0; i < GRID * GRID; i++) {
-        int id = order[i];
-        SDL_Rect src = { (id % GRID) * tileW, (id / GRID) * tileH, tileW, tileH };
-        SDL_Rect dst = { (i % GRID) * tileW, (i / GRID) * tileH, tileW, tileH };
-        SDL_RenderCopy(ren, img, &src, &dst);
+    for (int y = 0; y < GRID; y++) {
+        for (int x = 0; x < GRID; x++) {
+
+            int id = order[y][x];
+
+            SDL_Rect src = {
+                (id % GRID) * tileW,
+                (id / GRID) * tileH,
+                tileW,
+                tileH
+            };
+
+            SDL_Rect dst = {
+                x * tileW,
+                y * tileH,
+                tileW,
+                tileH
+            };
+
+            SDL_RenderCopy(ren, img, &src, &dst);
+        }
     }
 }
 
-// -------------------- SWAP --------------------
-void swapTiles(int a, int b) {
-    int tmp = order[a];
-    order[a] = order[b];
-    order[b] = tmp;
+// -------- SWAP (MATRICE) --------
+void swapTiles(int x1, int y1, int x2, int y2) {
+    int tmp = order[y1][x1];
+    order[y1][x1] = order[y2][x2];
+    order[y2][x2] = tmp;
 }
 
-// -------------------- TIMER --------------------
+// -------- TIMER --------
 void showTimer() {
     Uint32 now = SDL_GetTicks();
     int sec = (now - startTime) / 1000;
     printf("Time: %d sec\r", sec);
 }
 
-// -------------------- WIN ANIMATION SIMPLE --------------------
+// -------- WIN ANIMATION --------
 void winAnimation() {
     for (int i = 0; i < 20; i++) {
         SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
