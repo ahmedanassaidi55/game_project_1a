1a4eeb0d01b4a04db501c52140152a24367e202b
ahmedanassaidi55 <saidiahmedanas6@gmail.com>
2026-04-13 17:21:13 +0000

    added remaining functionality to background, needs further testing

--- source/background.c
+++ source/background.c
@@ -1,3 +1,5 @@
+#include "background.h"
+game_clock clk;
 void init_background(Background *bg,SDL_Renderer *renderer){
 	SDL_Surface *s = IMG_Load("assets/images/mapbg.png");
 	if(!s){
@@ -39,6 +41,7 @@ void init_background(Background *bg,SDL_Renderer *renderer){
 		
 		
 	}
+	clk.start = SDL_GetTicks()/1000;
 }
 int tilemap[TILE_HEIGHT][TILE_WIDTH] =0;
 void load_tilemap(char *tm_file){
@@ -84,4 +87,98 @@ void display_background(Background* bg,SDL_Renderer *renderer){
 	SDL_RenderCopy(renderer, bg->objects[i].base,NULL,&(SDL_Rect){ScreenX,ScreenY,bg->objects[i].position.w,bg->objects[i].position.h});}
 	}
 }
+void scroll_background(Background *bg, enum direction d, int step){
+	int diag_mvt = 0.7071; // to keep constant speed in diagonal directions
+	switch(d){
+	case up:
+	bg->offsetY = bg->offsetY-step<0?0:bg->offsetY-step;
+	break;
+	case up_right:
+	bg->offsetY = bg->offsetY-step*diag_mvt<0?0:bg->offsetY-step*diag_mvt;
+	bg->offsetX = bg->offsetX+step*diag_mvt>(BACKGROUND_WIDTH-SCREEN_WIDTH)?BACKGROUND_WIDTH-SCREEN_WIDTH:bg->offsetX+step*diag_mvt;
+	break;
+	case right:
+	bg->offsetX = bg->offsetX+step>(BACKGROUND_WIDTH-SCREEN_WIDTH)?BACKGROUND_WIDTH-SCREEN_WIDTH:bg->offsetX+step;
+	break;
+	case down_right:
+	bg->offsetY = bg->offsetY+step*diag_mvt>(BACKGROUND_HEIGHT-SCREEN_HEIGHT)?BACKGROUND_HEIGHT-SCREEN_HEIGHT:bg->offsetY+ step*diag_mvt;
+	bg->offsetX = bg->offsetX+step*diag_mvt<(BACKGROUND_WIDTH-SCREEN_WIDTH)?BACKGROUND_WIDTH-SCREEN_WIDTH:bg->offsetX+step*diag_mvt;
+	break;
+	case down:
+	bg->offsetY = bg->offsetY+step>(BACKGROUND_HEIGHT-SCREEN_HEIGHT)?BACKGROUND_HEIGHT-SCREEN_HEIGHT:bg->offsetY+step;
+	break;
+	case down_left:
+	bg->offsetY = bg->offsetY+step*diag_mvt>(BACKGROUND_HEIGHT-SCREEN_HEIGHT)?BACKGROUND_HEIGHT-SCREEN_HEIGHT:bg->offsetY+ step*diag_mvt;
+	bg->offsetX = bg->offsetX-step*diag_mvt<0?0:bg->offsetX-step*diag_mvt;
+	break;
+	case left:
+	bg->offsetX = bg->offsetX-step<0?0:bg->offsetX-step;
+	break;
+	case left_up:
+	bg->offsetY = bg->offsetY-step*diag_mvt<0?0:bg->offsetY-step*diag_mvt;
+	bg->offsetX = bg->offsetX-step*diag_mvt<0?0:bg->offsetX-step*diag_mvt;
+	break;
+	}
+}
+int is_blocked(int futureX, int futureY){
+	return tilemap[(int)futureX/TILE_WIDTH][(int)futureY/TILE_HEIGHT] == 1;
+}
+void interact(int x,int y,interactable * objects){
 
+for( interactable it = objects; it<objects+OBJ_COUNT;it++){
+	if(!it->is_interactedwith){
+		int dist = sqrt(pow(x - it->position.x, 2) + pow(y - it->position.y, 2));
+		if(dist<INTERACT_RAD){
+			it->is_interactedwith = 1;
+			tilemap[it->tilex][it->tiley] = 0;
+			}
+		}
+	}
+}
+void manage_time(game_clock *t,SDL_Renderer *renderer,TTF_Font *font){
+	t->elapsed = SDL_GetTicks()/1000-t->start;
+	int h = t->elapsed / 3600;
+    	int m = (t->elapsed % 3600) / 60;
+    	int s = t->elapsed % 60;
+    	char buff[10];
+    	sprintf(buff,"%02d:%02d:%02d",h,m,s);
+	if(strcmp(buff,t->disp_clock)){
+		strcpy(t->disp_clock,buff);
+		SDL_Surface* textSurface = TTF_RenderText_Blended(font, t->disp_clock, WHITE);
+		if(!textSurface){
+			printf("error creating surface @manage_time\n");
+			return;
+		}
+		if(t->texture)
+		SDL_DestroyTexture(t->texture);
+		t->texture = SDL_CreateTextureFromSurface(renderer,textSurface);
+		if(!t->texture){
+			printf("error creating texture @manage_time\n");
+			return;
+		}
+		SDL_FreeSurface(textSurface);
+	}
+}
+//clear renderer and render the frame only once per frame and not on every pass
+void display_tutorial(SDL_Renderer *renderer,tutorial_piece tut){
+	SDL_RenderCopy(renderer,tut.tut_display,NULL,tut.position);
+}
+void save_score(ScoreInfo s,char* time, char* score_file){
+	FILE *f = fopen(score_file,"a");
+	if(!f){
+	printf("failed to open score file @save_scores\n");
+	return;
+	}
+	fprintf(f,"%d|%s|%s\n",s.score,s.name,time);
+	fclose(f);
+	char sort_cmd[100];
+	sprintf(sort_cmd,"sort -o %s %s",score_file,score_file); 
+	if(system(sort_cmd)==-1){
+		printf("failed to sort @save_scores\n");
+		return;
+	}
+}
+//already implemented in helper.c, visit helper.c @455 to 533 for detailed implementation, we were already evaluated for this menu so idt you guys care much
+void display_highscores(char * score_file,SDL_Renderer *renderer){
+	
+}
--- source/background.h
+++ source/background.h
@@ -5,6 +5,9 @@
 #include "decl.h"
 //const
 #define OBJ_COUNT 6  //will be changed later once map is finalized
+#define BACKGROUND HEIGHT 3200
+#define BACKGROUND WIDTH 2000
+#define INTERACT_RAD 25
 #define ELEM_HEIGHT 18
 #define ELEM_WIDTH 26
 #define TILE_HEIGHT 10
@@ -28,9 +31,11 @@ typedef struct {
 typedef struct {
     int start;
     int elapsed;
+    char disp_clock[10];
+    SDL_Texture *texture;
 }game_clock;
 typedef struct {
-    int time, score;
+    int score;
     char name[20];
 }ScoreInfo;
 typedef struct {
@@ -43,20 +48,20 @@ enum direction{up,up_right,right,down_right,down,down_left,left,up_left};
 extern Background bg;
 extern tutorial_piece tutorial[3];
 extern int tilemap[TILE_HEIGHT][TILE_WIDTH];
+extern game_clock clk;
 //functions
 void init_background(Background *bg,SDL_Renderer *renderer);
 void load_tilemap(char *tm_file); //loading and saving will be provided with a save file if loading a save
 void save_curr_tilemap(char *tm_file);//will instead load from a default file and create new save if new game
 void display_background(Background *bg,SDL_Renderer *renderer);
 void scroll_background(Background *bg, enum direction d, int step);
-void is_blocked(int futureX, int futureY, enum direction d);
+int is_blocked(int futureX, int futureY);
                                             // to check if player/enemy character can move to specific location
                                             // should be called with currentX+velX, currentY+velY
                                             // if it returns true, the specified location cannot be traveled to
-int is_interactive(int x, int y); //check if tile close to player is usable, to activate doors or pick up items
 void interact(int x,int y,interactable * objects); // updates interactable object's texture and changes the tile value in the matrix
-void manage_time(game_clock *t);
+void manage_time(game_clock *t,SDL_Renderer *renderer,TTF_Font *font);
 void display_tutorial(SDL_Renderer *renderer,tutorial_piece tut);
-void save_score(ScoreInfo s, char* score_file);
+void save_score(ScoreInfo s,char* time, char* score_file);
 void display_highscores(char * score_file,SDL_Renderer *renderer);
 #endif
