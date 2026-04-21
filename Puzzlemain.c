24f971d12085ee3cdb36995c1ad090745df968c3
trimechoumayna <vortexcipher85@gmail.com>
2026-04-21 07:26:15 +0100

    Update Puzzlemain.c

--- Puzzlemain.c
+++ Puzzlemain.c
@@ -1,140 +1,77 @@
 #include "puzzle.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
-// -------- GLOBAL VARIABLES --------
-SDL_Window *win;
-SDL_Renderer *ren;
-SDL_Texture *img;
+int main() {
 
-Tile tiles[GRID * GRID];
-int order[GRID][GRID];
+    // init SDL
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
 
-Uint32 startTime;
-int winGame = 0;
+    // إنشاء window
+    win = SDL_CreateWindow("Sliding Puzzle",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_W, SCREEN_H, 0);
 
-// -------- LOAD IMAGE --------
-SDL_Texture* loadImage(const char *path) {
-    SDL_Surface *tmp = IMG_Load(path);
-    if (!tmp) {
-        printf("Erreur image: %s\n", path);
-        exit(1);
-    }
+    // renderer
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, tmp);
-    SDL_FreeSurface(tmp);
-    return tex;
-}
+    // تحميل الصورة
+    img = loadImage("image.png");
 
-// -------- INIT PUZZLE (MATRICE) --------
-void initPuzzle() {
-    int tileW = SCREEN_W / GRID;
-    int tileH = SCREEN_H / GRID;
+    // init اللعبة
+    initPuzzle();
 
-    int index = 0;
+    SDL_Event e;
+    int running = 1;
 
-    // création ordonnée
-    for (int y = 0; y < GRID; y++) {
-        for (int x = 0; x < GRID; x++) {
+    // boucle principale
+    while (running) {
 
-            tiles[index].id = index;
+        while (SDL_PollEvent(&e)) {
 
-            tiles[index].rect.x = x * tileW;
-            tiles[index].rect.y = y * tileH;
-            tiles[index].rect.w = tileW;
-            tiles[index].rect.h = tileH;
+            if (e.type == SDL_QUIT)
+                running = 0;
 
-            order[y][x] = index;
-            index++;
+            // click souris
+            if (e.type == SDL_MOUSEBUTTONDOWN) {
+
+                int x, y;
+
+                // نحولو click → case
+                getGridFromMouse(e.button.x, e.button.y, &x, &y);
+
+                // نحرك القطعة
+                moveTile(x, y);
+            }
         }
-    }
 
-    // shuffle matrice
-    srand(time(NULL));
-
-    for (int y = 0; y < GRID; y++) {
-        for (int x = 0; x < GRID; x++) {
-
-            int ry = rand() % GRID;
-            int rx = rand() % GRID;
-
-            int tmp = order[y][x];
-            order[y][x] = order[ry][rx];
-            order[ry][rx] = tmp;
-        }
-    }
-
-    startTime = SDL_GetTicks();
-}
-
-// -------- CHECK WIN --------
-int checkWin() {
-    int expected = 0;
-
-    for (int y = 0; y < GRID; y++) {
-        for (int x = 0; x < GRID; x++) {
-            if (order[y][x] != expected)
-                return 0;
-            expected++;
-        }
-    }
-    return 1;
-}
-
-// -------- RENDER PUZZLE --------
-void renderPuzzle() {
-    int tileW = SCREEN_W / GRID;
-    int tileH = SCREEN_H / GRID;
-
-    for (int y = 0; y < GRID; y++) {
-        for (int x = 0; x < GRID; x++) {
-
-            int id = order[y][x];
-
-            SDL_Rect src = {
-                (id % GRID) * tileW,
-                (id / GRID) * tileH,
-                tileW,
-                tileH
-            };
-
-            SDL_Rect dst = {
-                x * tileW,
-                y * tileH,
-                tileW,
-                tileH
-            };
-
-            SDL_RenderCopy(ren, img, &src, &dst);
-        }
-    }
-}
-
-// -------- SWAP (MATRICE) --------
-void swapTiles(int x1, int y1, int x2, int y2) {
-    int tmp = order[y1][x1];
-    order[y1][x1] = order[y2][x2];
-    order[y2][x2] = tmp;
-}
-
-// -------- TIMER --------
-void showTimer() {
-    Uint32 now = SDL_GetTicks();
-    int sec = (now - startTime) / 1000;
-    printf("Time: %d sec\r", sec);
-}
-
-// -------- WIN ANIMATION --------
-void winAnimation() {
-    for (int i = 0; i < 20; i++) {
-        SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+        // render
         SDL_RenderClear(ren);
-
-        SDL_Rect r = { 100 - i*2, 100 - i*2, 600 + i*4, 400 + i*4 };
-        SDL_RenderCopy(ren, img, NULL, &r);
-
+        renderPuzzle();
         SDL_RenderPresent(ren);
-        SDL_Delay(50);
+
+        // check win
+        if (checkWin()) {
+            printf("YOU WIN!\n");
+            winAnimation();
+            break;
+        }
+
+        showTimer();
+
+        SDL_Delay(16); // ~60 FPS
     }
+
+    // nettoyage
+    SDL_DestroyTexture(img);
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
 }
